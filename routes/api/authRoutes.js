d1bc4422ac262a421b79f891509711d65b9f2c05
mdrifat59 <mdrifatulislam59@gmail.com>
2024-06-10 11:04:56 +0600

    Registration post mathod validation

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -4,5 +4,17 @@ const _ = express.Router()
 _.get("/registration", function(req, res){
     res.send("done")
 })
+_.post("/registration", function(req, res){
+    let {name,email,password} = req.body
+     if(!name){
+        res.send("Name required")
+     }else if(!email){
+        res.send("Email required")
+     }else if (!password){
+        res.send("Password required")
+     }else{
+        res.send("Done")
+     }
+})
 
 module.exports = _ 
