76d34e330c81e6a5935cda915eddae442797dbef
mdrifat59 <mdrifatulislam59@gmail.com>
2024-07-03 12:56:24 +0600

    Login route or controller create and logic develop

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -2,10 +2,12 @@ const express = require("express")
 const _ = express.Router() 
  const registrationController = require ("../../Controllers/registrationController")
 const otpController = require("../../Controllers/otpController")
+const loginController = require("../../Controllers/loginController")
  
 
 _.post("/registration", registrationController)
 _.post("/otpverify", otpController)
+_.post("/login", loginController)
 
  
 
